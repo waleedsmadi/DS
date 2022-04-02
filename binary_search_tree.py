06a2789a9f6bc9b8c68b28aae8636d35116c5fad
waleed smadi <waleed0smadi@outlook.com>
2022-04-02 23:33:34 +0300

    finish remove branch

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -27,3 +27,27 @@ class BinarySearchTree:
         # put the element on the right side
         elif element > root.value:
             root.right = self.insert(root.right, element)
+
+    def get_min(self, root):
+        if not root.left:
+            return root
+        return self.get_min(root.left)
+
+    def remove(self, root, element):
+        if not root:
+            return root
+        if element < root.value:
+            root.left = self.remove(root.left, element)
+        elif element > root.value:
+            root.right = self.remove(root.right, element)
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            else:
+                val = self.get_min(root.right).value
+                root.value = val
+                root.right = self.remove(root.right, val)
+
+        return root

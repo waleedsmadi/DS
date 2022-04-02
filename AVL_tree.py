6f9c8a015ff6889747cfbcf21916304817aab8d0
waleed smadi <waleed0smadi@outlook.com>
2022-04-03 01:11:04 +0300

    finish remove branch

--- AVL_tree.py
+++ AVL_tree.py
@@ -92,3 +92,50 @@ class AVLTree:
                 return self.r_rotation(root)
 
         return root
+
+    def remove(self, root, element):
+        """ to remove an element from the tree """
+
+        # searching about the element
+        if not root:
+            return root
+        elif element < root.value:
+            root.left = self.remove(root.left, element)
+
+        elif element > root.value:
+            root.right = self.remove(root.right, element)
+
+        # found the element
+        else:
+            if not root.left:  # right child case
+                return root.right
+            elif not root.right:  # left child case
+                return root.left
+
+            else:  # two children case
+                val = self.get_min(root.right).value
+                root.value = val
+                root.right = self.remove(root.right, val)
+
+        # update the height
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+
+        balance_factor = self.get_balance(root)
+
+        if balance_factor < -1:  # means it's left rotation
+            if self.get_balance(root.right) <= 0:  # means it's left-left rotation
+                return self.l_rotation(root)
+
+            else:  # means it's left-right rotation
+                root.right = self.r_rotation(root.right)
+                return self.l_rotation(root)
+
+        if balance_factor > 1:  # means it's right rotation
+            if self.get_balance(root.left) >= 0:  # means it's right-right rotation
+                return self.r_rotation(root)
+
+            else:  # means it's right-left rotation
+                root.left = self.l_rotation(root.left)
+                return self.r_rotation(root)
+
+        return root
